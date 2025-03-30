5f0b49ac668285514160474abe051e4a02f58873
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-03-31 03:49:57 +0530

    Updated

--- source_src/config/cloudinaryConfig.js
+++ source_src/config/cloudinaryConfig.js
@@ -5,7 +5,8 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({
     cloud_name: CLOUDINARY_CLOUD_NAME,
     api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET
+    api_secret: CLOUDINARY_API_SECRET,
+    secure: true
 
 });
 
