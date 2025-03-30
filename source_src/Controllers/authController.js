7430360eb649a46bbee43136bffd7e297145b144
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-03-31 03:54:53 +0530

    Updated

--- source_src/Controllers/authController.js
+++ source_src/Controllers/authController.js
@@ -11,6 +11,8 @@ async function login(req, res){
         res.cookie("authToken", response.token, {
             httpOnly : true,
             secure: true,
+            sameSite: "none", 
+            domain: "deliverpizzasite.netlify.app",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         
