f62d733996ba6c24ef5f6758827ad330184263a3
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-04-15 02:58:15 +0530

    Added Chatbot to track order

--- source_src/Controllers/authController.js
+++ source_src/Controllers/authController.js
@@ -10,7 +10,7 @@ async function login(req, res){
 
         res.cookie("authToken", response.token, {
             httpOnly : true,
-            secure: false,
+            secure: true,
             sameSite: "none", 
             path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000
